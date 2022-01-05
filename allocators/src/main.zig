310551166d30b58298ae94ef3b769bd0233137e8
mathiasmagnusson <mathiasmagnussons@gmail.com>
2022-01-05 22:34:13 +0100

    Add tests

--- allocators/src/main.zig
+++ allocators/src/main.zig
@@ -3,7 +3,7 @@ const testing = std.testing;
 
 pub const ArenaAllocator = @import("arena.zig");
 
-test "arena allocator" {
+test "two ints" {
     var arena = ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     var allocator = arena.allocator();
@@ -15,4 +15,27 @@ test "arena allocator" {
     try testing.expect(j.* == 1337);
     allocator.destroy(i);
     allocator.destroy(j);
+}
+
+test "big allocations" {
+    var arena = ArenaAllocator.init(testing.allocator);
+    defer arena.deinit();
+    var allocator = arena.allocator();
+
+    const sizes = [_]usize{ 8, 4096, 23_478_894, 374_387, 2_481_274, 1_000_000_000, 1, 1 };
+    var buffers: [sizes.len][]u8 = undefined;
+
+    for (sizes) |n, i| {
+        buffers[i] = try allocator.alloc(u8, n);
+    }
+
+    for (buffers) |buf, i| {
+        std.mem.set(u8, buf, @intCast(u8, i));
+    }
+
+    for (buffers) |buf, i| {
+        for (buf) |c| {
+            try testing.expect(c == i);
+        }
+    }
 }
